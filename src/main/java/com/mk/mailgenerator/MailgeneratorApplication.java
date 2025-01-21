dd8b608c4e7b71fa77448d4fdef3edd922351935
Arkady Sokolovsky <arrkadique@gmail.com>
2025-01-21 09:41:19 +0100

    Initial commit

---
+++ src/main/java/com/mk/mailgenerator/MailgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.mk.mailgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailgeneratorApplication.class, args);
+	}
+
+}
