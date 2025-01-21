47c4bbc2b5a513d075f4d867dec0a7778bfd0085
Arkady Sokolovsky <arrkadique@gmail.com>
2025-01-21 13:30:36 +0100

    Renamed main class

--- src/main/java/com/mk/mailgenerator/MailGeneratorApplication.java
+++ src/main/java/com/mk/mailgenerator/MailGeneratorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MailgeneratorApplication {
+public class MailGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MailgeneratorApplication.class, args);
+		SpringApplication.run(MailGeneratorApplication.class, args);
 	}
 
 }
